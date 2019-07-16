53c094e14f07dff02480800a043ff55fe2e08d46
Tim Williams <timothymwilliams88@gmail.com>
2019-07-16 18:28:44 -0400

    dump

--- FallWebsite/js/app.js
+++ FallWebsite/js/app.js
@@ -1,7 +1,36 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
+const mobileToggle = document.querySelector('.btn_toggle_nav');
+const menu = document.querySelector('.nav_main ul');
+const anchors = menu.querySelectorAll('li > a');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.dir(email.value);
+    // If an email is not entered before "sumbit" is clicked
+  if (email.value == ''){
+    // Alert will pop up stating below text
+    alert("Please Enter an Email.")
+  } else {
+    // If email is present console will collect data
+    console.dir(email.value);
+    alert(`We will be sending updates to ${email.value}`)
+    email.value = '';
+  }
+});
+
+
+   // Responsive menu
+mobileToggle.addEventListener('click', (e) => {
+   // Logging event in the Console
+  console.log(e);
+   // toggling the CSS active class on .nav_main ul
+  menu.classList.toggle('active');
+});
+
+anchors.forEach(anchor => {
+   // Adding click lister to every Anchor
+  anchor.addEventListener('click', (e) => {
+    // removing the CSS active class from .nav_main ul on click
+    menu.classList.remove('active');
+  });
 })
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fall
+
+This is a website for the my Metal band.
+
+You will see a bio for the band and listen to our music.
+
+to run open index.html.
